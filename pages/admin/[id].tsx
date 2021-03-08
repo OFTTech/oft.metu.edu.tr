0edf58518d983c5eeca5f9ba7c82270590833ea6
isaacturing112358 <tobuba2@gmail.com>
2021-03-08 17:14:10 +0300

    redirect to admin

---
+++ pages/admin/[id].tsx
@@ -0,0 +1,24 @@
+import DefaultErrorPage from 'next/error'
+
+export async function getStaticProps(context) {
+    if (context.params.id == process.env.WP_BEARER) {
+        return {
+            redirect: {
+                destination: process.env.WP_SITE,
+                permanent: false
+            }
+        }
+    }
+    return {props: {}}
+}
+
+export async function getStaticPaths() {
+    return {
+        paths: [],
+        fallback: true
+    };
+}
+
+export default function AdminID() {
+    return <DefaultErrorPage statusCode={404}/>
+}
